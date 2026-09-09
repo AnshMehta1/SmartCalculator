3023dbf741081cf7a732e3d33e150d8b870961c2
AnshMehta1 <ansh.mehta522007@gmail.com>
2026-09-09 17:01:13 +0530

    feat: Implement a Calculable interface to give a common method to all calculator operations

---
+++ src/main/java/com/savoira/Calculable.java
@@ -0,0 +1,14 @@
+package com.savoira;
+
+/**
+ * Defines an object that can perform a calculation.
+ */
+public interface Calculable {
+
+    /**
+     * Performs the calculation.
+     *
+     * @return the result of the calculation
+     */
+    double calculate();
+}
